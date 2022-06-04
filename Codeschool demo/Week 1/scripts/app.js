855f48bf53c623622f5edb99edd3779291a14fd5
Lizzy Wiley <Lizzyrho@gmail.com>
2022-06-04 17:54:51 -0500

    added code along files

---
+++ Codeschool demo/Week 1/scripts/app.js
@@ -0,0 +1,19 @@
+// Javascript review!
+
+// Is there a way to check if my JS is working properly?
+
+
+// We need a way to greet our users when they open our page..
+
+// How are we supposed to know our user's name? Is there a way to store it?
+
+// TRUE or FALSE: the keyword 'var' is acceptable to use when declaring variables?
+
+// We need to make sure the first letter of thier name is capitalized after they enter it.
+
+// It would help to know more about our users; How old are they, What is thier favorite thing to do on the weekend? etc...
+
+// We might need to add quick statements in our JS file so other developers can see what we have / are working on.
+
+// Our JS needs some spice to it..What else can we add?
+
